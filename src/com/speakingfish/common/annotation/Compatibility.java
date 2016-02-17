53747d7df36295920df09bc951dd5abe894273f6
speaking-fish <speakingfish@gmail.com>
2016-02-17 09:13:51 +0300

    1.5 compatibility

--- src/com/speakingfish/common/annotation/Compatibility.java
+++ src/com/speakingfish/common/annotation/Compatibility.java
@@ -1,10 +1,5 @@
 package com.speakingfish.common.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 public class Compatibility {
 /*
     @Target(ElementType.METHOD)
